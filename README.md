2f9fb665bbd72db75a03b9474d2d28781e6b8554
vidy.alfredo@shopee.com <vidy.alfredo@shopee.com>
2020-01-15 13:05:45 +0700

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 relies on geonearby
 https://github.com/strikeentco/geo-nearby
+
+# How to use
+const geo = require('geo-kecamatan');
+geo.nearBy(-6.8862572,107.523612) ; 
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const fs = require('fs')
 const Geo = require('geo-nearby');
-
 const files = fs.readFileSync('./data/kecamatan.json', {encoding: 'utf-8'});
 let arr = JSON.parse(files);
-
 let dataSet = Geo.createCompactSet(arr);
 dataSet = dataSet.sort((a,b) => a.g - b.g)
 const geo = new Geo(dataSet, { sorted: false });
 
-
-const result111 = geo.nearBy(-6.8862572,107.523612, [250, 30000]) ; //10 km
-console.log(result111)
-
-exports.module = geo;
+exports.module = {
+    nearBy: (lat, long) => {
+        return geo.nearBy(lat, long, [250, 30000]) ; 
+    }
+};
