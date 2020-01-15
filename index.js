1e5bd190ae6ed64192bbbd20b877c8d14d2d1385
vidy.alfredo@shopee.com <vidy.alfredo@shopee.com>
2020-01-15 13:02:16 +0700

    fix bug

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ const Geo = require('geo-nearby');
 const files = fs.readFileSync('./data/kecamatan.json', {encoding: 'utf-8'});
 let arr = JSON.parse(files);
 
-arr = arr.map(a => {
-    return [a.latitude, a.longitude, a.name]
-});
-
 let dataSet = Geo.createCompactSet(arr);
 dataSet = dataSet.sort((a,b) => a.g - b.g)
-const geo = new Geo(dataSet, { sorted: true });
+const geo = new Geo(dataSet, { sorted: false });
+
+
+const result111 = geo.nearBy(-6.8862572,107.523612, [250, 30000]) ; //10 km
+console.log(result111)
 
 exports.module = geo;
-
-const result111 = geo.nearBy(-6.9264539,107.5944051, [250, 30000]) ; //10 km
-const body = JSON.stringify(dataSet)
-fs.writeFileSync('./kecamatan.json', body)
-console.log(result111)
