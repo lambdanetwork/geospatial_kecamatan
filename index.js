2112d28748acdba51faf3d70ffa102e851e8b532
vidy.alfredo@shopee.com <vidy.alfredo@shopee.com>
2020-01-15 13:11:21 +0700

    add database as javascript

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const fs = require('fs')
 const Geo = require('geo-nearby');
-const files = fs.readFileSync('./data/kecamatan.json', {encoding: 'utf-8'});
-let arr = JSON.parse(files);
+let arr = require('./data/kecamatan')
 let dataSet = Geo.createCompactSet(arr);
 dataSet = dataSet.sort((a,b) => a.g - b.g)
 const geo = new Geo(dataSet, { sorted: false });
-
-exports.module = {
+let GEO = {
     nearBy: (lat, long) => {
         return geo.nearBy(lat, long, [250, 30000]) ; 
     }
 };
+module.exports = GEO
